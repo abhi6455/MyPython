92948b0fb840bd0d25b4342276220d68beeb781e
abhi6455 <117516661+abhi6455@users.noreply.github.com>
2022-11-12 17:56:24 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,32 @@
+Creating the String 
+str1='India'
+print(str1)
+
+str2='"India"'
+print(str2)
+
+Print of String
+print("Length of the string is :",(len(str1)))
+
+Multiline String
+str3='''He
+will not be 
+available 
+for the meeting '''
+print(str3) 
+
+Concatenate 
+print(str1+str2)
+
+Compare
+print(str1==str2)
+
+Print each character in the list 
+for char in str1:
+ print(char)
+
+ Lower and upper case 
+
+str4='Indianrockseverywhere'
+print(str4.upper())
+print(str4.lower())
